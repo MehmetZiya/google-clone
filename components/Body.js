f2b48a049a401c0f1d446d87d5eff897994bca67
MehmetZiya <mehmetziya82@gmail.com>
2022-12-14 13:13:28 +0100

    created search input form and search result pages

--- components/Body.js
+++ components/Body.js
@@ -1,7 +1,18 @@
 import Image from 'next/image'
 import { SearchIcon, MicrophoneIcon } from '@heroicons/react/solid'
+import { useRouter } from 'next/router'
+import { useRef } from 'react'
 
 const Body = () => {
+  const router = useRouter()
+  const searchInputRef = useRef(null)
+  const search = (e) => {
+    e.preventDefault()
+    const term = searchInputRef.current.value
+    if (!term.trim()) return
+    router.push(`/search?term=${term.trim()}`)
+  }
+
   return (
     <form className='flex flex-col items-center mt-40'>
       <Image
@@ -10,14 +21,21 @@ const Body = () => {
         width={300}
         height={100}
         className={` hover:bg-gray-100`}
+        priority
       />
       <div className='flex w-full mt-5 mx-auto max-w-[90%] border border-gary-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl'>
         <SearchIcon className='h-5 text-gray-500 mr-3' />
-        <input type='text' className='flex-grow focus:outline-none' />
+        <input
+          type='text'
+          ref={searchInputRef}
+          className='flex-grow focus:outline-none'
+        />
         <MicrophoneIcon className='h-5 text-gray-500' />
       </div>
       <div className='flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center'>
-        <button className='btn'>Google Search</button>
+        <button className='btn' onClick={search}>
+          Google Search
+        </button>
         <button className='btn'>I&apos;m Feeling Lucky</button>
       </div>
     </form>
---
+++ pages/search/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import React from 'react'
+import SearchHeader from '../../components/SearchHeader'
+
+const search = () => {
+  return (
+    <div>
+      <Head>
+        <title>Search Page</title>
+      </Head>
+      {/** Header */}
+      <SearchHeader />
+
+      {/** Search Results */}
+    </div>
+  )
+}
+
+export default search
